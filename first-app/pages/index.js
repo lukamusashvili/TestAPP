2a3b1cb1a9679dc6e590b74fdfd405359c5fbb1e
lukamusashvili <57059505+lukamusashvili@users.noreply.github.com>
2021-03-01 17:14:11 +0400

    v1
    v1

---
+++ first-app/pages/index.js
@@ -0,0 +1,12 @@
+const Index = () => (
+    <html>
+    <header>
+        <title>Title Goes here</title>
+    </header>
+    <div>
+        This is a test app.
+    </div>
+    </html>
+)
+
+export default Index;
